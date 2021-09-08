cf17f51ef1fabb95d537a01849a34006f00c16f0
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2021-09-08 15:14:35 -0300

    front-end modifications

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,7 @@ const CompanyCrontroller = require('./controller/CompanyCrontroller');
 
 const routes = express.Router();
 
-
-routes.get('/', (req, res) => {
-    res.send('Salveeeeeeeeeeeee')
-});
-
-
 routes.get('/company', CompanyCrontroller.index);
-routes.post('/company', CompanyCrontroller.store);
 routes.post('/company/:id', CompanyCrontroller.create);
+
 module.exports = routes;
